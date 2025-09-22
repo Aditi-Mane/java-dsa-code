83bf931fe92bf8355902e656baf9c09aa3ef2382
Aditi-Mane <143419307+Aditi-Mane@users.noreply.github.com>
2025-09-22 20:52:44 +0530

    Printing Divisors: Mathematical Observation method

--- src/Basic_Maths/PrintDivisors.java
+++ src/Basic_Maths/PrintDivisors.java
@@ -1,6 +1,7 @@
 package Basic_Maths;
 
 public class PrintDivisors {
+    //method 1: brute force
     public static void divisors(int n){
         for(int i=1; i <= n; i++){
             if(n % i == 0){
@@ -8,7 +9,19 @@ public class PrintDivisors {
             }
         }
     }
+    //method 2: mathematical observation
+    public static void divisors2(int n){
+        for(int i=1;i<=Math.sqrt(n);i++){
+            if(n%i==0){
+                System.out.print(i+" ");
+                if((n/i)!=i){
+                    System.out.print(n/i+" ");
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        PrintDivisors.divisors(36);
+        PrintDivisors.divisors2(36);
     }
 }
