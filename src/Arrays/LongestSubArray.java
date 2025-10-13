58ab5f6e54aebe3e16f0583766730ca3f2fb1630
Aditi-Mane <143419307+Aditi-Mane@users.noreply.github.com>
2025-10-14 00:38:03 +0530

    Arrays: Longest SubArray with sum K: positives

---
+++ src/Arrays/LongestSubArray.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+public class LongestSubArray {
+
+    //sliding window approach: TC - O(n); SC - 0(1)
+    static int sub(int[] arr, int k){
+        int start = 0, maxLength = 0, sum = 0;
+        for(int end = 0; end<arr.length;end++){
+            sum = sum + arr[end];
+            while(sum > k){
+                sum = sum - arr[start];
+                start++;
+            }
+            if(sum == k){
+                int currLength = end - start + 1;
+                maxLength = Math.max(currLength, maxLength);
+            }
+        }
+        return maxLength;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int k = 9;
+        int length = sub(arr, k);
+        System.out.println("The length of the longest sub-array with k sum is "+length);
+    }
+}
