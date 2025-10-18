ff65f68bd6aaff63296fc29f3c4c650736d22531
Aditi-Mane <143419307+Aditi-Mane@users.noreply.github.com>
2025-10-18 18:10:58 +0530

    Arrays: Longest SubArray with sum K: positives & negatives (using prefixSum & HashMap)

--- src/Arrays/LongestSubArray.java
+++ src/Arrays/LongestSubArray.java
@@ -1,5 +1,7 @@
 package Arrays;
 
+import java.util.HashMap;
+
 public class LongestSubArray {
 
     //brute force approach: TC - O(n^2); SC - O(1)
@@ -33,10 +35,40 @@ public class LongestSubArray {
         }
         return maxLength;
     }
+
+    //prefix sum + hashmap approach ( for +ve & -ve ) : TC - O(n); SC - O(n)
+    static int sub3(int[] arr, int k) {
+        HashMap<Integer, Integer> prefixMap = new HashMap<>();
+        int sum = 0;
+        int maxLength = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+
+            // Case 1: subarray starts from index 0
+            if (sum == k) {
+                maxLength = i + 1;
+            }
+
+            // Case 2: if (sum - k) exists as a key then currLength is i - sum-k's index
+            if (prefixMap.containsKey(sum - k)) {
+                int prevIndex = prefixMap.get(sum - k);
+                int currLength = i - prevIndex;
+                maxLength = Math.max(maxLength, currLength);
+            }
+
+            // Store sum in map only if not already present (we want the first occurrence)
+            if (!prefixMap.containsKey(sum)) {
+                prefixMap.put(sum, i);
+            }
+        }
+
+        return maxLength;
+    }
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
-        int k = 9;
-        int length = sub2(arr, k);
+        int[] arr = {10, -10, 20, -5, 5, -15, 10};
+        int k = 10;
+        int length = sub3(arr, k);
         System.out.println("The length of the longest sub-array with k sum is "+length);
     }
 }
