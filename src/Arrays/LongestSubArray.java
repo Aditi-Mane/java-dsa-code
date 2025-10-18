b378470902a8e5f942bfa917875e607f0bd255b0
Aditi-Mane <143419307+Aditi-Mane@users.noreply.github.com>
2025-10-18 14:08:16 +0530

    Arrays: Longest SubArray with sum K: positives (brute force)

--- src/Arrays/LongestSubArray.java
+++ src/Arrays/LongestSubArray.java
@@ -2,6 +2,21 @@ package Arrays;
 
 public class LongestSubArray {
 
+    //brute force approach: TC - O(n^2); SC - O(1)
+    static int sub2(int[] arr, int k){
+        int maxLength = 0;
+        for(int i=0;i<arr.length;i++){
+            int sum = 0;
+            for(int j=i;j<arr.length;j++){
+                sum = sum + arr[j];
+                if(sum == k){
+                    maxLength = Math.max(j - i + 1, maxLength);
+                }
+            }
+        }
+        return maxLength;
+    }
+
     //sliding window approach: TC - O(n); SC - 0(1)
     static int sub(int[] arr, int k){
         int start = 0, maxLength = 0, sum = 0;
@@ -21,7 +36,7 @@ public class LongestSubArray {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
         int k = 9;
-        int length = sub(arr, k);
+        int length = sub2(arr, k);
         System.out.println("The length of the longest sub-array with k sum is "+length);
     }
 }
