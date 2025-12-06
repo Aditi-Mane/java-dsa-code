57bf694e24b06aa6e7496d56a6a76d565b3d2d8c
Aditi-Mane <143419307+Aditi-Mane@users.noreply.github.com>
2025-12-06 14:36:09 +0530

    Arrays: Maximum SubArray sum (Kadane's Algorithm) -> Optimal Approach

--- src/Arrays/KadaneAlgorithm.java
+++ src/Arrays/KadaneAlgorithm.java
@@ -14,9 +14,22 @@ public class KadaneAlgorithm {
         }
         return max;
     }
+
+    //Optimal method (Kadane's Algorithm): TC - O(n); SC - O(1)
+    static int subArray2(int[] arr){
+        int max = Integer.MIN_VALUE, sum = 0;
+        for(int i=0;i<arr.length;i++){
+            sum = sum + arr[i];
+            max = Math.max(sum, max);
+            if(sum<0){
+                sum = 0;
+            }
+        }
+        return max;
+    }
     public static void main(String[] args) {
         int[] arr = {-2,-3,4,-1,-2,1,5,-3};
-        int max = subArray(arr);
+        int max = subArray2(arr);
         System.out.println(max);
     }
 }
